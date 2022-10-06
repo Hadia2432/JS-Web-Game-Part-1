fcae6c3da6030a92ff97efe4a0304dfdf10ab2dd
unknown <ghadia140@gmail.com>
2022-10-06 14:50:14 -0400

    changes made in index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function tile(url, left, bottom, width, height){
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
+greenCharacter.style.left ='100px'
+greenCharacter.style.bottom ='100px'
 document.body.append(greenCharacter)
 
 let pineTree = document.createElement('img')
